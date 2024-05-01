ea67f30189e8141cdf813d5988f7602d61317083
RomanDovgii <romandovgiiwork@gmail.com>
2024-05-01 21:26:13 +0300

    Completes first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,7 +1,53 @@
-import {encoded, translations} from './data.js'
+import {encoded, translations} from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const idCounter = {};
+
+const decodeData = (encodedData, vocabulary) => {
+    const decodedData = encodedData.reduce((acc, datum) => {        
+        const holder = {};
+
+        Object.keys(datum).forEach((element) => {
+            if (
+                element === 'groupId' || 
+                element === 'service' || 
+                element === 'formatSize' || 
+                element === 'ca'
+            ) {
+                holder[element] = datum[element];
+            } else {
+                holder[element] = vocabulary[datum[element]] ? vocabulary[datum[element]] : datum[element];
+                idCounter[datum[element]] ? idCounter[datum[element]] += 1 : idCounter[datum[element]] = 1;
+            }
+        })
+        return acc = [ ...acc, holder];
+    }, []);
+
+    
+
+    return decodedData;
+}
+
+const getUniqueIds = (idEncounters) => {
+    const uniqueIds = Object.keys(idEncounters).reduce((acc, element) => {
+        if (idEncounters[element] === 1) {
+            acc.push(element)
+        }
+
+        return acc;
+    }, []);
+
+    return uniqueIds;
+}
+
+const decodedData = decodeData(encoded, translations);
+const uniqueIds = getUniqueIds(idCounter);
+
+console.log(decodedData);
+console.log(    
+    `Список уникальных id: ${uniqueIds}`
+);
+// console.log("Let's rock");
+// console.log(encoded, translations);
 
 
 
